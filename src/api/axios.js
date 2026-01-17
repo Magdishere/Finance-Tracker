2c456cf56ffd5278b250df7093ad6d114d2c909c
Magd El Zalameh <Magd El Zalameh>
2026-01-17 10:35:03 +0200

    fixing the auth/me

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,21 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
+export default axios.create({
   baseURL: process.env.REACT_APP_API_URL || "https://finance-tracker-api-53xq.onrender.com/api",
   withCredentials: true
 });
-
-api.interceptors.request.use(
-  (config) => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (user && user.token) {
-      config.headers["Authorization"] = `Bearer ${user.token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-export default api;
